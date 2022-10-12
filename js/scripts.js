590ff0434b8538ab303cd81a24756b2debd4fc51
Tee-rogers <90728261+Tee-rogers@users.noreply.github.com>
2022-10-12 10:21:23 -0400

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+
+//function that returns the value 1
+function number() {
+  return 1
+}
+
+var ah = number();
+
+console.log(ah);
+
+///function that returns whatever number value inputted
+
+function october(value) {
+  return value
+}
+
+var ahh = october(2);
+
+console.log(ahh);
+
+
+
+///function that return the result of number inputed + 10
+function november(value){
+  return value + 10
+}
+var birthday = november(3);
+
+console.log(birthday);
+
+
+//expression that adds the result of all three functions called
+birthday + ahh + ah
+
+var altogether = birthday + ahh + ah
+console.log(altogether)
